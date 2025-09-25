a426bea627396ef9c473eb157a75c2172e9ebb28
kassidykegler-art <kassidy.kegler@gmail.com>
2025-09-25 16:13:14 -0400

    Add files via upload

---
+++ info.py
@@ -0,0 +1,75 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "I'm Bugs Bunny. I work at ACME. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/bugs-bunny-44b857281/"
+my_github_url = "https://github.com/bugs-bunny"
+my_email_address = "bugs_bunny@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Never',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "CS 1331", "MATH 1554", "CS 2050"], 
+    "names":["Intro to CS", "Intro to OOP", "Linear Algebra", "Discrete Mathematics"], 
+    "semester_taken":["1st", "2nd", "3rd", "3rd"],
+    "skills":["I like snakes less now", "Java's not just coffee", "Basically row reduction", "It should stay discrete"],
+    }
+experience_data = {
+    "Professional Basketball Player": (["- Played on the same team as Micheal Jordan",
+                                                                          "- MVP of the Tune Squad"],"Images/ball.jpeg"),
+    "Created the Electric Carrot Peeler":(["- Sold 100,000 units",
+                                                           "- Made carrots #1 vegetable in the world"],"Images/peel.jpeg"),
+
+}
+
+projects_data = {
+    "Duck Season": "Established the first Duck Season",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 80,
+    "C": 50,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Fluent",
+    "Spanish": "Fluent",
+}
+leadership_data = {
+    "Principal at Looniversity": (["- Mentored Buster Bunny"],"Images/buster.jpeg"),
+
+}
+activity_data={
+    "Mrs. Porkybunny's": ["- Created a semi-successful carrot cake business", 
+            ]
+}
